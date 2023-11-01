bc73209c02c75647d117719fb95efb226ee57892
iMeisa <62470487+iMeisa@users.noreply.github.com>
2023-11-01 15:45:29 +0200

    Fixed CurrentUrl function

--- client/src/variables/Url.ts
+++ client/src/variables/Url.ts
@@ -3,12 +3,12 @@ import GetPosition from "../functions/strings/CharPosition.ts";
 export default function CurrentUrl() {
 
     const href = window.location.href
-    const path = window.location.pathname
+    // const path = window.location.pathname
     const url = href.substring(0, GetPosition(href, "/", 3))
 
     if (url === 'http://127.0.0.1:5173') {
         return 'http://127.0.0.1:8080'
     }
 
-    return href.substring(0, href.length-path.length)
+    return url
 }

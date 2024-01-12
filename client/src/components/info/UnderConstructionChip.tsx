0d013161d5638e6882d7b893664032ef9ba7ca57
iMeisa <62470487+iMeisa@users.noreply.github.com>
2024-01-12 14:45:31 +0200

    Added tooltip to under construction chip

--- client/src/components/info/UnderConstructionChip.tsx
+++ client/src/components/info/UnderConstructionChip.tsx
@@ -1,6 +1,8 @@
-import {Chip} from "@mui/material";
+import {Chip, Tooltip} from "@mui/material";
 import WarningIcon from "@mui/icons-material/Warning";
 
 export default function UnderConstructionChip() {
-    return <Chip icon={<WarningIcon color="warning"/>} label="Under Construction"/>
+    return <Tooltip title="Feature not fully functional yet">
+        <Chip icon={<WarningIcon color="warning"/>} label="Under Construction"/>
+    </Tooltip>
 }

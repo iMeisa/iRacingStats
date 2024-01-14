ee6b6327840d8cf23089e9434ad0278b01f7cda1
iMeisa <62470487+iMeisa@users.noreply.github.com>
2024-01-14 20:34:54 +0200

    Removed autoheight from StatsGrid

--- client/src/components/data/grid/StatsGrid.tsx
+++ client/src/components/data/grid/StatsGrid.tsx
@@ -1,21 +1,32 @@
 import {DataGrid, DataGridProps, GridNoRowsOverlay} from "@mui/x-data-grid";
 import StatsGridToolbar from "./StatsGridToolbar.tsx";
 import Box from "@mui/material/Box";
+// import {useEffect, useState} from "react";
 
 export default function StatsGrid(props: DataGridProps) {
+    // const emptyRow: GridRowsProp = [{
+    //     id: 0,
+    // }]
+    // const [rows, setRows] = useState(emptyRow)
+    //
+    // useEffect(() => {
+    //     if( props.rows.length > 0 ) setRows(props.rows)
+    // }, [props.rows]);
+
     return (
         <Box sx={{ width: '100%' }}>
             <DataGrid
                 {...props}
-                autoHeight
+                // autoHeight
                 slots={{
                     noRowsOverlay: GridNoRowsOverlay,
                     toolbar: StatsGridToolbar,
                 }}
+                // rows={rows}
                 loading={props.loading}
                 sx={{
                     '--DataGrid-overlayHeight': '50px',
-                    maxHeight: '75vh',
+                    maxHeight: '72vh',
                 }}
                 disableColumnMenu
                 pageSizeOptions={[]}

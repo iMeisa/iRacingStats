12083db0fb492095e4f59716cd7b522c2d93f1e2
iMeisa <62470487+iMeisa@users.noreply.github.com>
2024-01-30 10:08:25 +0200

    Added tooltip to club logo

--- client/src/variables/Url.ts
+++ client/src/variables/Url.ts
@@ -5,7 +5,7 @@ export default function CurrentUrl() {
     const href = window.location.href
     const url = href.substring(0, GetPosition(href, "/", 3))
 
-    if (url === 'http://127.0.0.1:5173') {
+    if (['http://127.0.0.1:5173', 'http://localhost:5173'].includes(url)) {
         return 'http://127.0.0.1:8080'
     }
 
